a57a55b382b023f40b7d9a46a3f65d44578f1f95
mouri <tt.mouri@gmail.com>
2019-03-05 18:47:49 +0900

    for-pool

--- db.js
+++ db.js
@@ -18,28 +18,24 @@ if (process.env.NODE_ENV === "production") {
 
 exports.connection = connection;
 
-function handleDisconnect() {
-  console.log('INFO.CONNECTION_DB: ');
-  connection = mysql.createConnection(connection);
+var pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'cm-act-mysql',
+    user: 'docker',
+    password: 'docker',
+    database: 'ccwebdb'
+});
 
-  connection.connect((err) => {   　//DB接続テスト用。アクセス成功でメッセージ返す。
-      if (err) {
-          console.log('Error connecting to DB');
-          console.error(err);
-      }
-      else { console.log('Connection established'); }
+pool.getConnection(function(err, connection) {
+  if (err) throw err; // not connected!
+
+  // Use the connection
+  connection.query('select * from cm_aclg where al_idactv = 1', function (error, results, fields) {
+    // When done with the connection, release it.
+    connection.release();
+    // Handle error after the release.
+    if (error) throw error;
+    // Don't use the connection here, it has been returned to the pool.
   });
-
-  //error('PROTOCOL_CONNECTION_LOST')時に再接続
-  connection.on('error', function (err) {
-      console.log('ERROR.DB: ', err);
-      if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-          console.log('ERROR.CONNECTION_LOST: ', err);
-          handleDisconnect();
-      } else {
-          throw err;
-      }
-  });
-}
-
-handleDisconnect();
+});
+exports.pool = pool;
